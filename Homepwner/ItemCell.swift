d2262c6e9f9b6ab3a50e3f4d2586c5237b494e42
Ivan Soriano <ivansoriano@gmail.com>
2016-06-04 15:30:21 +0200

    make custom TableViewCell for Item

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Ivan Soriano on 6/4/16.
+//  Copyright © 2016 Guavabot. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -19,6 +19,9 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
     }
     
     @IBAction func addNewItem(sender: AnyObject) {
@@ -47,10 +50,11 @@ class ItemsViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let item = itemStore.allItems[indexPath.row]
         
-        let tableViewCell = tableView.dequeueReusableCellWithIdentifier("UiTableViewCell", forIndexPath: indexPath)
-        tableViewCell.textLabel?.text = item.name
-        tableViewCell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        return tableViewCell
+        let itemCell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
+        itemCell.nameLabel.text = item.name
+        itemCell.valueLabel.text = "$\(item.valueInDollars)"
+        itemCell.serialLabel.text = item.serialNumber
+        return itemCell
     }
     
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
