0a2c6828808921dd831a3b34e0ce15da64c94863
Ivan Soriano <ivansoriano@gmail.com>
2016-06-06 00:50:38 +0200

    add DetailViewController displaying item in Stack

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -78,4 +78,14 @@ class ItemsViewController: UITableViewController {
     override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
         itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowItem" {
+            if let row = tableView.indexPathForSelectedRow?.row {
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destinationViewController as! DetailViewController
+                detailViewController.item = item
+            }
+        }
+    }
 }
